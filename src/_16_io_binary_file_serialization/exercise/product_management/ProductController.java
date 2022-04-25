bd3f8e4d14a2dd08261aab1ab6f5c8f119564d9f
Kien1204 <nguyendtrungkien1204@gmail.com>
2022-04-25 11:28:19 +0700

    update

--- src/_16_io_binary_file_serialization/exercise/product_management/ProductController.java
+++ src/_16_io_binary_file_serialization/exercise/product_management/ProductController.java
@@ -1,11 +1,48 @@
 package _16_io_binary_file_serialization.exercise.product_management;
 
+import com.sun.javafx.iio.gif.GIFImageLoaderFactory;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class ProductController {
+    private static List<Product> productList = new ArrayList<>();
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        productList.add(new Product(1, "iphone", "apple", 14000000));
+        productList.add(new Product(2, "Nokia Lumia", "Nokia", 15000000));
+        productList.add(new Product(3, "SamSung Galaxy", "SamSung", 16000000));
+        productList.add(new Product(4, "Xiaomi Pro", "Xiaomi", 17000000));
+        System.out.println("Input path");
+        String path = scanner.nextLine();
+
+        int choice = -1;
+        while (choice != 4) {
+            System.out.println("Menu");
+            System.out.println("1. display product:");
+            System.out.println("2. add new one: ");
+            System.out.println("3. find product :");
+            System.out.println("4.exit");
+
+            choice = Integer.parseInt(scanner.nextLine());
+            switch (choice) {
+                case 1:
+                    displayProductList(path);
+                    break;
+                case 2:
+                    addNewProduct(path);
+                    break;
+                case 3:
+                    searchProductInfo(path);
+                    break;
+            }
+        }
+    }
+
     public static void writeFile(String path, List<Product> products) {
         File file = new File(path);
         FileOutputStream os = null;
@@ -44,55 +81,31 @@ public class ProductController {
         return productList;
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        List<Product> productList = new ArrayList<>();
-        productList.add(new Product(1, "iphone", "apple", 14000000));
-        productList.add(new Product(2, "Nokia Lumia", "Nokia", 15000000));
-        productList.add(new Product(3, "SamSung Galaxy", "SamSung", 16000000));
-        productList.add(new Product(4, "Xiaomi Pro", "Xiaomi", 17000000));
-        System.out.println("Input path");
-        String path = scanner.nextLine();
-
-        int choice = -1;
-        while (choice != 4) {
-            System.out.println("Menu");
-            System.out.println("1. display product:");
-            System.out.println("2. add new one: ");
-            System.out.println("3. find product :");
-            System.out.println("4.exit");
-
-            choice = Integer.parseInt(scanner.nextLine());
-            switch (choice) {
-                case 1:
-                    displayProductList(path);
-                    break;
-                case 2:
-                    productList.add(addNewProduct());
-                    writeFile(path, productList);
-                    break;
-                case 3:
-                    searchProductInfo(path);
-                    break;
-            }
-        }
-    }
-
-    public static Product addNewProduct() {
-        Scanner scanner= new Scanner(System.in);
+    public static void addNewProduct(String path) {
+        List<Product> productList = readFile(path);
         System.out.println("Input id :");
         int id = Integer.parseInt(scanner.nextLine());
-        System.out.println("Input name :");
-        String name = scanner.nextLine();
-        System.out.println("Input brand : ");
-        String brand = scanner.nextLine();
-        System.out.println("Input price");
-        int price = scanner.nextInt();
-        Product product = new Product(id,name,brand,price);
-        return product ;
+        boolean check = true;
+        for (Product data : productList) {
+            if (data.getId() == id) {
+                System.out.println("id da ton tai");
+                check = false;
+                break;
+            }
+        }
+        if (check) {
+            int id1 = Integer.parseInt(scanner.nextLine());
+            System.out.println("Input name :");
+            String name = scanner.nextLine();
+            System.out.println("Input brand : ");
+            String brand = scanner.nextLine();
+            System.out.println("Input price");
+            int price = scanner.nextInt();
+            Product product = new Product(id, name, brand, price);
+            productList.add(product);
+            writeFile(path, productList);
+        }
     }
-
-
     public static void displayProductList(String path) {
         List<Product> productList = readFile(path);
         for (Product item : productList) {
---
+++ src/_17_string_regex/practice/validate_crawl_song_list/CrawlExample.java
@@ -0,0 +1,38 @@
+package _17_string_regex.practice.validate_crawl_song_list;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CrawlExample {
+    public static <Matcher> void main(String[] args) {
+        try {
+            URL url = new URL("https://www.nhaccuatui.com/bai-hat/nhac-tre-moi.html");
+            // open the stream and put it into BufferedReader
+            Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
+            scanner.useDelimiter("\\Z");
+            String content = scanner.next();
+            // close scanner
+            scanner.close();
+            // remove all new line
+            content = content.replaceAll("\\n+", "");
+            // regex
+            Pattern p = Pattern.compile("name_song\">(.*?)</a>");
+            Matcher m = (Matcher) p.matcher(content);
+            while (((java.util.regex.Matcher) m).find()) {
+                System.out.println(((java.util.regex.Matcher) m).group(1));
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
